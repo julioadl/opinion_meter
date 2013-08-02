0d0d6db192c59ae6bc52f425cd8a0b49b1009c77
julioadl <julioadl@gmail.com>
2013-08-02 11:51:03 -0500

    checks exact words and classifies it according to a codebook

--- classification.py
+++ classification.py
@@ -14,5 +14,21 @@ for row in data:
         word = row.split()[:-1]
         new_dict.append(word[0])
 
-for item in new_dict:
-    print item
+code_1 = ['save', 'safe', 'risk', 'casualties']
+code_2 = ['abroad']
+code_3 = ['money', 'expense', 'cost', 'expensive']
+code_4 = ['preparation', 'training', 'prepared']
+code_5 = ['hurt', 'kills']
+code_6 = ['target', 'precise', 'accura']
+code_7 = ['surv', 'reconn']
+code_8 = ['terrori']
+code_9 = ['tech']
+code_10 = ['secre', 'spy']
+code_book = {1:code_1, 2:code_2, 3:code_3, 4:code_4, 5:code_5, 6:code_6, 7:code_7, 8:code_8, 9:code_9, 10:code_10}
+
+for item in new_dict = []
+    for list in code_book:
+        for term in code_book[list]:
+            if item in code_book[list]:
+                print item, list
+            
