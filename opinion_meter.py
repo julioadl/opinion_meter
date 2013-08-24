71f285a9aa0f76c29eb8126d16441612e5c0f797
julioadl <julioadl@gmail.com>
2013-08-24 15:44:03 +0100

    Generates a score for an individual responding four different answers for the same question. Includes the capacity to go for a positive dictionary and a negative one

--- opinion_meter.py
+++ opinion_meter.py
@@ -5,10 +5,11 @@ import sys
 
 #open dictionary
 
-afinfile = open(sys.argv[1])
+afinfile_good = open(sys.argv[1]) #dictionary for the question with positive connotation
+afinfile_bad = open(sys.argv[2])  #dictionary for the questions with negative connotation
 
 # open and import into the CSV. It skips the first line as it's the header
-csv_file_object = csv.reader(open(sys.argv[2], "rb"))
+csv_file_object = csv.reader(open(sys.argv[3], "rb"))
 header = csv_file_object.next()
 data = []
 
@@ -24,51 +25,83 @@ data = np.array(data)
 question = {}
 for individual in range(len(data)):
     response = []
-    response.append(data[individual,17])    #picks up the item of the numpy array
-    response.append(data[individual,18])
-    response.append(data[individual,19])
-    response.append(data[individual,20])
-    response.append(data[individual,22])
-    response.append(data[individual,23])
-    response.append(data[individual,24])
-    response.append(data[individual,25])
+    response.append(data[individual,37])    #picks up the item of the numpy array
+    response.append(data[individual,38])    #the first four items correspond to the positive 
+    response.append(data[individual,39])
+    response.append(data[individual,40])
+    response.append(data[individual,42])    #the last four correspond to the ones with negs
+    response.append(data[individual,43])
+    response.append(data[individual,44])
+    response.append(data[individual,45])
     question[data[individual,0]] = response #adds the items of the numpy array into one individual respone
 
 #new dictionary
-dictionary = []
-negatives = ['no', 'No', 'NO', 'Nothing', 'nothing', 'none', 'None','NONE']
-neutrals = ['Nothing', 'nothing']
+positive = [] #dictionary for the positives
+negative = [] #dictionary for the negatives
 
-for line in afinfile:
-    dictionary.append(line)
+#prepares the dictionary for the positives
+for line in afinfile_good:
+    positive.append(line)
 
 #scores
-scores = {}
-for item in dictionary:
+scores_pos = {}
+for item in positive:
     term, score = item.split()
     #term = term.decode('utf-8')
-    scores[term] = int(score)
+    scores_pos[term] = int(score)
+
+#prepares the dictionary for the negative
+for line in afinfile_bad:
+    negative.append(line)
+
+#scores
+scores_neg = {}
+for item in negative:
+    term, score = item.split()
+    #term = term.decode('utf-8')
+    scores_neg[term] = int(score)
+
+negatives = ['no', 'No', 'NO', 'none', 'NONE', 'None', 'not good', 'nothing good']
 
 #compare with dictionary
 for individual in question:
     general_score = 0
-    
+    dictionary = 0
+
     for answer in question[individual]:
+        
         score_feeling = 0
         words = answer.split()
-
-        for term in scores:
-            for item in words: 
-                if term == item:
-                    score_feeling += scores[term]
+        
+        if dictionary < 3:
+            for term in scores_pos:
+                for item in words: 
+                    if term == item:
+                        score_feeling += scores_pos[term]
                 
-        if answer in negatives:
-            score_feeling = 0
+            if answer in negatives:
+                score_feeling = 0
 
-        if score_feeling > 0:
-            general_score += 1
-        elif score_feeling < 0:
-            general_score -= 1
+            if score_feeling > 0:
+                general_score += 1
+            elif score_feeling < 0:
+                general_score -= 1
+
+            dictionary += 1
+
+        elif dictionary > 3:
+            for term in scores_neg:
+                for item in words: 
+                    if term == item:
+                        score_feeling += scores_neg[term]
+                
+            if answer in negatives:
+                score_feeling = 0
+
+            if score_feeling > 0:
+                general_score += 1
+            elif score_feeling < 0:
+                general_score -= 1
 
     question[individual].append(general_score)
     print general_score
